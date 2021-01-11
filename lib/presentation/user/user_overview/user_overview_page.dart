780f84f386a180a747d15d713b7ac4fabb676f65
djordje-maslic <djooordje@gmail.com>
2021-01-11 18:33:39 +0100

    user4

--- lib/presentation/user/user_overview/user_overview_page.dart
+++ lib/presentation/user/user_overview/user_overview_page.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class UserOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child:Text(''));
   }
 }
